c7e3c7a9dfbb4394e7474c86cea911d8d1880974
Filip Kubacki <filip.kubacki@emo-link.com>
2023-08-17 14:41:46 +0100

    test default

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def loadData():
         for row in spamreader:
             tmp = row[0].split(",")
             res.append((int(tmp[0]), tmp[1]))
-    return res[:100]
+    return res
 
 with open('results.csv', 'w') as f:
     f.write('source,target,score\n')
