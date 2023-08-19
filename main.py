eaf571f039671433e248043461f31c10a0020016
Filip Kubacki <filip.kubacki@emo-link.com>
2023-08-19 15:42:58 +0100

    run test

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def loadData():
         for row in spamreader:
             tmp = row[0].split(",")
             res.append(int(tmp[0]))
-    return res
+    return res[:10000]
 
 
 with open('results.csv', 'w') as f:
